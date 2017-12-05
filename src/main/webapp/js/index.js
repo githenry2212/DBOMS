5392c64c5c616244ad714c48314d4d7947133f66
杨帆 <henry2212@163.com>
2017-12-05 09:02:38 +0800

    index update

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -1,2 +1,14 @@
 (function(window,undefined){
+
+    function preventLinkEvent(){
+        var menus = window.document.querySelectorAll(".oms-menu a");
+        for(var idx = 0;idx<menus.length;idx++){
+            menus[idx].onclick = function(e){
+                e.preventDefault();
+            }
+        }
+    }
+
+    preventLinkEvent();
+
 })(window)
