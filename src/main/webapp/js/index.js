3e0f40c3ca0472e8f0980d2b1adb08ae7970f883
yangfan <henry2212@163.com>
2017-12-06 00:05:47 +0800

    update

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -1,5 +1,2 @@
-var omsApp = angular.module('omsApp',['ngRoute'])
-
-omsApp.controller('nestedCtrl', function($scope) {
-    $scope.message= "Nested App Controller Displayed";
-});
+// noinspection JSUnresolvedFunction
+var omsApp = angular.module('omsApp', ['ngRoute'])
