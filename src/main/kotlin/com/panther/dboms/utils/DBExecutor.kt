35b8748826430aeba8dea449d1ff15e148c5986f
杨帆 <henry2212@163.com>
2017-12-18 10:25:32 +0800

    DBExecutor optmz

--- src/main/kotlin/com/panther/dboms/utils/DBExecutor.kt
+++ src/main/kotlin/com/panther/dboms/utils/DBExecutor.kt
@@ -7,8 +7,24 @@ package com.panther.dboms.utils
 import java.sql.Connection
 import java.sql.PreparedStatement
 import java.sql.ResultSet
+import java.sql.SQLException
 
 class DBExecutor(private val conn: Connection) {
+
+    fun query(sql: String, setter: PreparedStatement.() -> Unit): QueryResult {
+        var ps: PreparedStatement? = null
+        var rs: ResultSet? = null
+        return try {
+            ps = conn.prepareStatement(sql)
+            ps.setter()
+            rs = ps.executeQuery()
+            QueryResult(ps, rs)
+        } catch (e: SQLException) {
+            rs?.close()
+            ps?.close()
+            throw e
+        }
+    }
 
     fun <R> query(sql: String, setter: PreparedStatement.() -> Unit, mapper: ResultSet.() -> R?): R? {
         var ps: PreparedStatement? = null
@@ -43,6 +59,17 @@ class DBExecutor(private val conn: Connection) {
             return ps.executeBatch().sum()
         } finally {
             ps?.close()
+        }
+    }
+}
+
+class QueryResult(private val ps: PreparedStatement, private val rs: ResultSet) {
+    infix fun <R> mapper(rsMapper: ResultSet.() -> R?): R? {
+        return try {
+            rs.rsMapper()
+        } finally {
+            rs.close()
+            ps.close()
         }
     }
 }
